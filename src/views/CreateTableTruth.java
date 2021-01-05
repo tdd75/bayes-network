75aba13db3f199f33cf31601a9a9ec1f06f5be73
Duy Tran <tranducduy7520@gmail.com>
2021-01-05 14:55:46 +0700

    update

--- src/views/CreateTableTruth.java
+++ src/views/CreateTableTruth.java
@@ -1,11 +1,11 @@
 package views;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import com.brunomnsilva.smartgraph.graph.Graph;
 
-import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
@@ -15,14 +15,45 @@ import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TableColumn.CellEditEvent;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Pair;
 
 public class CreateTableTruth {
 
+    public class Data {
+        private String t, f;
+
+        public Data(String t, String f) {
+            this.t = t;
+            this.f = f;
+        }
+
+        public String getT() {
+            return t;
+        }
+
+        public void setT(String t) {
+            this.t = t;
+        }
+
+        public String getF() {
+            return f;
+        }
+
+        public void setF(String f) {
+            this.f = f;
+        }
+    }
+
+    public static Map<String, List<Data>> listData = new HashMap<>();
+
     public void CreateTable(Graph<String, String> g, String v) {
+//        if(listData != null)
+//        System.out.println(listData.get(0).getT());
         TableView<Integer> table = new TableView<>();
         Scene scene = new Scene(new Group());
         Stage stage = new Stage(StageStyle.DECORATED);
@@ -37,14 +68,15 @@ public class CreateTableTruth {
         for (int i = 0; i < countRows; i++) {
             table.getItems().add(i);
         }
-        // init listItem
+        // init list
         List<List<String>> listItem = new ArrayList<>();
         for (int j = 0; j < countParent; j++) {
             List<String> tmp = new ArrayList<>();
             listItem.add(tmp);
         }
-
+        // init value list
         for (int i = 0; i < countRows; i++) {
+            // listItem
             String s = Integer.toBinaryString(i);
             while (s.length() < countParent)
                 s = "0" + s;
@@ -54,24 +86,111 @@ public class CreateTableTruth {
                 else
                     listItem.get(j).add("T");
             }
-        }
-        System.out.println(listItem);
 
-        for (int k = 0 ; k<listVertex.size();++k) {
+        }
+//        System.out.println("size: " + listData.get(0).getF());
+        if(listData.get(v) == null)
+        {
+            List<Data> tmp = new ArrayList<>();
+            for (int i = 0; i < countRows; i++) {
+                Data data = new Data("", "");
+                tmp.add(data);
+            }
+            listData.put(v, tmp);
+        }
+
+        for (int k = 0; k < listVertex.size(); ++k) {
             TableColumn<Integer, String> col = new TableColumn<>(listVertex.get(k));
             int finalK = k;
             col.setCellValueFactory(cellData -> {
                 Integer rowIndex = cellData.getValue();
                 return new ReadOnlyStringWrapper(listItem.get(finalK).get(rowIndex));
             });
+            col.setOnEditCommit(new EventHandler<CellEditEvent<Integer, String>>() {
+                @Override
+                public void handle(CellEditEvent<Integer, String> event) {
+                    System.out.println(event.getNewValue());
+                }
+            });
+            col.setCellFactory(TextFieldTableCell.forTableColumn());
             table.getColumns().add(col);
         }
 
+        // data
         TableColumn vertexTarget = new TableColumn<>(v);
-        TableColumn trueColumn = new TableColumn<>("T");
-        TableColumn falseColumn = new TableColumn<>("F");
+        // true column
+        TableColumn<Integer, String> trueColumn;
+        trueColumn = new TableColumn<>("T");
+        trueColumn.setMinWidth(50);
+        trueColumn.setCellValueFactory(cellData -> {
+            Integer rowIndex = cellData.getValue();
+            return new ReadOnlyStringWrapper(listData.get(v).get(rowIndex).getT());
+        });
+        trueColumn.setOnEditCommit(new EventHandler<CellEditEvent<Integer, String>>() {
+            @Override
+            public void handle(CellEditEvent<Integer, String> event) {
+                System.out.println("Column T: Row = " + event.getTablePosition().getRow() + "; Value = " + event.getNewValue());
+                listData.get(v).get(event.getTablePosition().getRow()).setT(event.getNewValue());
+            }
+        });
+        trueColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        // false column
+        TableColumn<Integer, String> falseColumn;
+        falseColumn = new TableColumn<>("F");
+        falseColumn.setMinWidth(50);
+
+        falseColumn.setCellValueFactory(cellData -> {
+            Integer rowIndex = cellData.getValue();
+            return new ReadOnlyStringWrapper(listData.get(v).get(rowIndex).getF());
+        });
+        falseColumn.setOnEditCommit(new EventHandler<CellEditEvent<Integer, String>>() {
+            @Override
+            public void handle(CellEditEvent<Integer, String> event) {
+                System.out.println("Column F: Row = " + event.getTablePosition().getRow() + "; Value = " + event.getNewValue());
+                listData.get(v).get(event.getTablePosition().getRow()).setF(event.getNewValue());
+            }
+        });
+        falseColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        // add to table
         vertexTarget.getColumns().addAll(trueColumn, falseColumn);
-        table.getColumns().addAll(vertexTarget);
+        table.getColumns().add(vertexTarget);
+
+//        TableColumn trueColumn = new TableColumn<>("T");
+//        TableColumn falseColumn = new TableColumn<>("F");
+//        trueColumn.setMinWidth(50);
+//        falseColumn.setMinWidth(50);
+//
+//        trueColumn.setCellValueFactory(
+//                new PropertyValueFactory<Data, Double>("T"));
+//        trueColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+////        table.setItems(FXCollections.observableArrayList(listData));
+//        trueColumn.setCellValueFactory(new PropertyValueFactory<Data, String>("t"));
+//
+//        trueColumn.setOnEditCommit(
+//                new EventHandler<CellEditEvent<Data, Double>>() {
+//                    @Override
+//                    public void handle(CellEditEvent<Data, Double> t) {
+//                        System.out.println("Column T: Row = " + t.getTablePosition().getRow() + "; Value = " + t.getNewValue());
+//                    }
+//                }
+//        );
+//        falseColumn.setCellValueFactory(
+//                new PropertyValueFactory<Data, Double>("F"));
+//        falseColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+//        falseColumn.setOnEditCommit(
+//                new EventHandler<CellEditEvent<Data, Double>>() {
+//                    @Override
+//                    public void handle(CellEditEvent<Data, Double> t) {
+//
+//                        System.out.println("Column T: Row = " + t.getTablePosition().getRow() + "; Value = " + t.getNewValue());
+//                    }
+//                }
+//        );
+//
+//
+//        vertexTarget.getColumns().addAll(trueColumn, falseColumn);
+//        table.getColumns().addAll(vertexTarget);
 
         final VBox vbox = new VBox();
         vbox.setSpacing(5);
