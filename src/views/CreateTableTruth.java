3e994493741cb470ff370e66f13441c78ec3b21d
Duy Tran <tranducduy7520@gmail.com>
2021-01-05 15:00:47 +0700

    fix bug

--- src/views/CreateTableTruth.java
+++ src/views/CreateTableTruth.java
@@ -133,7 +133,6 @@ public class CreateTableTruth {
                 listData.get(v).get(event.getTablePosition().getRow()).setT(event.getNewValue());
             }
         });
-        trueColumn.setCellFactory(TextFieldTableCell.forTableColumn());
 
         // false column
         TableColumn<Integer, String> falseColumn;
@@ -151,7 +150,6 @@ public class CreateTableTruth {
                 listData.get(v).get(event.getTablePosition().getRow()).setF(event.getNewValue());
             }
         });
-        falseColumn.setCellFactory(TextFieldTableCell.forTableColumn());
         // add to table
         vertexTarget.getColumns().addAll(trueColumn, falseColumn);
         table.getColumns().add(vertexTarget);
